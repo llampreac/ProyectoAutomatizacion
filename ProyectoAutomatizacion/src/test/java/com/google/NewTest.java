a1d5f730b57c2cb119873d389f2400cb2dd84306
leydi lamprea <leydi lamprea@Leydi>
2021-07-24 10:14:18 -0500

    First commit

---
+++ ProyectoAutomatizacion/src/test/java/com/google/NewTest.java
@@ -0,0 +1,29 @@
+package com.google;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	
+	public static WebDriver driver;
+	
+  @Test
+  public void f() {
+	  System.setProperty("webdriver.chrome.driver", "./src/test/resources/drivers/chrome/chromedriver.exe");
+	  driver = new ChromeDriver();
+
+	  driver.get("https://www.google.com/");
+	  driver.manage().window().maximize();
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
